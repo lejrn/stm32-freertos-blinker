e95bf96611717bd6436a8c8345177c0d84bcc29e
TLRN <lrn.tl.dv@gmail.com>
2025-05-24 02:40:32 +0300

    Add cross-compilation build system
    - CMakeLists.txt with FreeRTOS integration and ARM toolchain support
    - ARM GCC toolchain configuration for STM32F103C8T6
    - STM32F103 linker script with 64K Flash / 20K RAM memory layout
    - Automated .hex file generation for firmware flashing
    
    Build system supports both firmware cross-compilation and
    host-side unit testing with separate build directories.

---
+++ cmake/arm-gcc.cmake
@@ -0,0 +1,19 @@
+set(CMAKE_SYSTEM_NAME Generic)
+set(CMAKE_SYSTEM_PROCESSOR arm)
+
+# Cross compiler settings
+set(CMAKE_C_COMPILER arm-none-eabi-gcc)
+set(CMAKE_CXX_COMPILER arm-none-eabi-g++)
+set(CMAKE_ASM_COMPILER arm-none-eabi-gcc)
+set(CMAKE_OBJCOPY arm-none-eabi-objcopy)
+set(CMAKE_OBJDUMP arm-none-eabi-objdump)
+set(CMAKE_SIZE arm-none-eabi-size)
+
+# Where to find libraries and include files
+set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
+set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_PACKAGE ONLY)
+
+# Don't run the linker during try_compile
+set(CMAKE_TRY_COMPILE_TARGET_TYPE STATIC_LIBRARY)
